e437addeec36f3e4e61aa8613c4a2f34567ea1ac
alvinrightback <muffin020@yahoo.com>
2018-04-01 08:31:29 +0800

    Change order to descending

--- application/models/Mobile_model.php
+++ application/models/Mobile_model.php
@@ -7,7 +7,7 @@ class Mobile_model extends CI_Model{
 		$this->db->select('*');
 		$this->db->from('auxillary');
 		$this->db->where('aux_group', 'product_category');
-		$this->db->order_by('aux_value', 'asc');
+		$this->db->order_by('aux_value', 'desc');
 		$query = $this->db->get();
 		if($query->num_rows() >0){
 			foreach($query->result() as $key1=>$row1){
@@ -16,6 +16,7 @@ class Mobile_model extends CI_Model{
 				$this->db->select('*');
 				$this->db->from('product');
 				$this->db->where('product_category_id', $row1->aux_id);
+				$this->db->order_by('name', 'desc');
 				$query = $this->db->get();
 
 				if($query->num_rows() >0){
