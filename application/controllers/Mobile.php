fc5bc3c40f72a5ce94d3deb32509e62b96e82045
alvinrightback <muffin020@yahoo.com>
2018-04-10 02:05:56 +0800

    added updated db

--- application/controllers/Mobile.php
+++ application/controllers/Mobile.php
@@ -23,6 +23,22 @@ class Mobile extends CI_Controller {
  		}
 	}
 
+	public function display_orders(){
+		$query = $this->mobile_model->display_orders();
+		if($query){
+			$data['status'] = 1;
+			$data['message'] = 'success';
+			$data['order_details'] = $query;
+			echo json_encode($data);
+		}
+		else{
+			$data['status'] = 0;
+			$data['message'] = 'failed';
+			$data['order_details'] = $query;
+			echo json_encode($data);
+ 		}
+	}
+
 	public function submit_orders(){
 
 		$query = $this->mobile_model->submit_orders();
