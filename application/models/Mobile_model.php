6b352213b6ea45187f4da19dc86f1c1f31ddb0a7
alvinrightback <muffin020@yahoo.com>
2018-04-14 11:17:30 +0800

    fixed get_menu

--- application/models/Mobile_model.php
+++ application/models/Mobile_model.php
@@ -4,7 +4,7 @@ class Mobile_model extends CI_Model{
 
 
 	public function get_menu(){
-		$this->db->select('*');
+		$this->db->select('aux_id, aux_value');
 		$this->db->from('auxillary');
 		$this->db->where('aux_group', 'product_category');
 		$this->db->order_by('aux_value', 'desc');
@@ -13,14 +13,14 @@ class Mobile_model extends CI_Model{
 			foreach($query->result() as $key1=>$row1){
 				$data[] = $row1;
 
-				$this->db->select('*');
+				$this->db->select('product_id, name, inventory_id');
 				$this->db->from('product');
 				$this->db->where('product_category_id', $row1->aux_id);
 				$this->db->order_by('name', 'desc');
 				$query = $this->db->get();
 
 				if($query->num_rows() >0){
-
+					$temp = array();
 					foreach($query->result() as $key2=>$row2){
 						$temp[] = $row2;
 						$temp[$key2]->availability = $this->get_minimum_inventory($row2->inventory_id);
